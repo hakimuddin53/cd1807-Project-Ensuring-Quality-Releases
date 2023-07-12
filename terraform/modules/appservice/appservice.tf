5e077463ee5aeac9f835f0b5b97531b7cd53a928
hakimuddin53 <hakimuddin752@gmail.com>
2023-07-12 22:10:08 +0800

    Added

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -11,4 +11,11 @@ resource "azurerm_linux_web_app" "test" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   service_plan_id     = azurerm_service_plan.test.id
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = 0
+  }
+  site_config {
+    always_on = false
+  }
 }
